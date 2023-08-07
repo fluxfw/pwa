d2135b2ca6c17734ae439aff1e3b8673612edfbe
fluxfw <fluxfw@bluewin.ch>
2023-08-07 11:35:54 +0200

    `flux-localization-api`

--- src/Localization/LOCALIZATIONS.mjs
+++ src/Localization/LOCALIZATIONS.mjs
@@ -7,13 +7,11 @@ export const LOCALIZATIONS = Object.freeze([
             "de-LI"
         ]),
         getTexts: async () => (await import("./TEXTS_DE.mjs")).TEXTS_DE,
-        label: "Deutsch",
         language: "de"
     },
     {
         "fallback-default": true,
         getTexts: async () => (await import("./TEXTS_EN.mjs")).TEXTS_EN,
-        label: "English",
         language: "en"
     }
 ].map(localization => Object.freeze(localization)));
--- src/Localization/Localization.mjs
+++ src/Localization/Localization.mjs
@@ -1,3 +1,3 @@
 /**
- * @typedef {{addModule: (module: string, localizations: {"fallback-default"?: boolean, "fallback-languages"?: string[], getTexts: () => Promise<{[key: string]: string}>, label: string, language: string}[]) => Promise<void>, getLanguage: (module: string) => Promise<{language: string}>, translate: (module: string, key: string, placeholders?: {[key: string]: string} | null) => Promise<string>}} Localization
+ * @typedef {{addModule: (module: string, localizations: {"fallback-default"?: boolean, "fallback-languages"?: string[], getTexts: () => Promise<{[key: string]: string}>, language: string}[]) => Promise<void>, getLanguage: (module: string) => Promise<{language: string}>, translate: (module: string, key: string, placeholders?: {[key: string]: string} | null) => Promise<string>}} Localization
  */
